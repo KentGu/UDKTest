58f9f72ce18199557fc8827bbf7804adcc311097
Kai Gu <kai.gu@hp.com>
2013-12-27 15:35:49 +0800

    updated

--- src/main/java/my/selenium/handler/XMLHandler.java
+++ src/main/java/my/selenium/handler/XMLHandler.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 import org.dom4j.Document;
 import org.dom4j.DocumentException;
+import org.dom4j.Element;
 import org.dom4j.Node;
 import org.dom4j.io.SAXReader;
 
@@ -17,13 +18,13 @@ import org.dom4j.io.SAXReader;
 public class XMLHandler {
 	public static void main (String[] args) throws DocumentException{
 		SAXReader sr = new SAXReader();
-		  Document doc = sr.read("config.xml");
-		  @SuppressWarnings("unchecked")
-		List<? extends Node> list = doc.selectNodes("//test");		  
-		  for(Node obj:list){
-		   
-		   Node el = (Node)obj;
-		   System.out.println(el.getPath());
-		  }
+		Document doc = sr.read("config.xml");
+		//List<Node> list = doc.selectNodes("//*");
+		Node node = doc.selectSingleNode("/testCases/module");
+		System.out.println(node.getPath());
+		//System.out.println(list.size());
+		//for (Node element:list){
+	//		System.out.println(element.getPath());
+		//}
 	}
 }
